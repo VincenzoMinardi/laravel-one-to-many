d4fb7fc59e4764f5346283a47f571766ead607ad
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-07-10 17:34:14 +0200

    add: type and collabs on type

--- database/migrations/2023_07_10_112502_create_types_table.php
+++ database/migrations/2023_07_10_112502_create_types_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('type', 50);
+            $table->string('collabs', 100);
         });
     }
 
