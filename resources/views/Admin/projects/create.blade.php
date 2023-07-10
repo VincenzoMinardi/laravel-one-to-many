9b056b07e82acfc30538f4832e16da986d9f5632
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-07-10 18:56:33 +0200

    delete comment

--- resources/views/Admin/projects/create.blade.php
+++ resources/views/Admin/projects/create.blade.php
@@ -1,18 +1,10 @@
 @extends('admin.layouts.base')
 
 @section('contents')
-
+<div class="container">
     <h1>Add new project</h1>
 
-    {{-- @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif --}}
+    
 
     <form method="POST" action="{{ route('admin.projects.store') }}" novalidate>
         @csrf
@@ -32,6 +24,23 @@
                 </div>
             @enderror
         </div>
+
+        <div class="mb-3">
+            <label for="type" class="form-label">Type</label>
+            <input
+                type="text"
+                class="form-control @error('type') is-invalid @enderror"
+                id="type"
+                name="type"
+                value="{{ old('type') }}"
+            >
+            @error('type')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+
 
         <div class="mb-3">
             <label for="url_image" class="form-label">date</label>
@@ -82,6 +91,23 @@
         </div>
 
         <div class="mb-3">
+            <label for="title" class="form-label">Collabs</label>
+            <input
+                type="text"
+                class="form-control @error('collabs') is-invalid @enderror"
+                id="collabs"
+                name="collabs"
+                value="{{ old('collabs') }}"
+            >
+            @error('collabs')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+
+
+        <div class="mb-3">
             <label for="content" class="form-label">description</label>
             <textarea
                 class="form-control @error('description') is-invalid @enderror"
@@ -97,5 +123,6 @@
 
         <button class="btn btn-primary">Save</button>
     </form>
+</div>
 
 @endsection
