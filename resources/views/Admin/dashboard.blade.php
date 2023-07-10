058f606c550659cbe61e2c83f9b737cc3a5348d4
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-07-10 18:56:22 +0200

    delete button

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -1,13 +1,10 @@
 @extends('admin.layouts.base')
 
 @section('contents')
+<div class="container">
+    <p class="text-center h1">Qui troverai tutti miei progetti riguardo al mio percorso fatto con Boolean</p>
+    
+    
+</div>
 
-<p class="text-center h1">Qui troverai tutti miei progetti riguardo al mio percorso fatto con Boolean</p>
-<button class="m-auto button-dashboard">
-    <a class="link" href="/admin/projects">Projects</a>
-    <div class="top"></div>
-    <div class="left"></div>
-    <div class="bottom"></div>
-    <div class="right"></div>
-</button>
 @endsection
